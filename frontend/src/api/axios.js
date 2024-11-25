ff4c93e5f5b7deb4ee772819122032f218dca7d4
Khurram Shahrukh <khurramshahrukh18@gmail.com>
2024-11-26 01:01:11 +0500

    removed console

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-console.log("🚀 ~ process.env.REACT_APP_API_BASE_URL:", process.env.REACT_APP_API_BASE_URL)
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL
 });
